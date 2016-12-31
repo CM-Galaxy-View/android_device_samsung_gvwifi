350902fdda286d9e4ca636817db0d60b97195fdf
Sean Hoyt <seanhoyt963@gmail.com>
2016-12-30 20:30:29 -0600

    I am a tablet

--- device.mk
+++ device.mk
@@ -21,6 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/samsung/gvwifi/overlay
 
+# Tablet
+PRODUCT_CHARACTERISTICS := tablet
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
